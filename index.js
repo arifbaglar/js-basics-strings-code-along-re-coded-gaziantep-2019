6e3449863301a5df2040b7720a628d3fc9757008
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-06-04 05:31:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const currentUser = "Grace Hopper";
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser+ "!" ;
 const upperCaseCurrentUser = currentUser.toUpperCase();
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+currentUser+"!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+upperCaseCurrentUser+"!";
 
